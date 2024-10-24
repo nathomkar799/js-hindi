23d1189e322235048c8df2fd5aa074cb585fe4cd
Omkar Nath <71923429+nathomkar799@users.noreply.github.com>
2024-10-24 09:33:06 +0000

    coversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Omkar"
+
+let bolleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof bolleanIsLoggedIn);
+// console.log(bolleanIsLoggedIn);
+
+// 1 => true; 0=> flase
+// "" => false
+// "Omkar" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
