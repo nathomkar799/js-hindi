077906c2cab21b4fd6c429320d4611ef6b9a08b7
Omkar Nath <71923429+nathomkar799@users.noreply.github.com>
2024-10-24 09:12:30 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs not browser
+
+console.log(3
+    +
+    3 //code readability should be high
+)
+
+console.log("Omkar");
+
+let name = "Omkar"
+let age = 25
+let isLoggedIn = false
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undegined => unassigned variable
+// symbol => unique 
+
+
+// object
+
+console.log(typeof null); //type of null is object
+console.log(typeof undefined); //type of undefined is undefined
