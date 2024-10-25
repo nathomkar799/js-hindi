6a2bace4734bd8c8a5b16a50126592715bd7b242
Omkar Nath <71923429+nathomkar799@users.noreply.github.com>
2024-10-25 08:38:04 +0000

    conversion part 2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,6 +24,58 @@ let bolleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// **********************Operations************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Omkar"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "2");//32//Never write code like this
+
+// console.log( (3 + 4) * 5 % 3);//2// Write your like this using pranthesis
+
+// console.log(true);//true
+// console.log(true);//1
+
+// console.log(+"");//0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
