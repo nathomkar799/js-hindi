c5472d9211d1ea100c5854c67d2d7dc4198e7bc7
Omkar Nath <71923429+nathomkar799@users.noreply.github.com>
2024-10-25 12:27:11 +0000

    compasrison of operatores

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,32 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);//true
+// console.log("02" > 1);//true
+
+// console.log(null > 0);//false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true//The reason is that an equality check == and comparisons ><>=<= work differently.---> Comparisons convert null to a number, treating it as 0.
+
+// console.log(undefined == 0);//false
+// console.log(undefined > 0);//false
+// console.log(undefined < 0);//false
+
+//need to avoid all the above 6 statements while coding.
+
+//===
+
+console.log("2" === 2);//false beacause triple === checks strictly the operands.
+
+
+
+
+
+
+
+
+
+
