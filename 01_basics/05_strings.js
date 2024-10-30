e98da41872eac6f58622a5e67469c6b86c7bc77c
Omkar Nath <71923429+nathomkar799@users.noreply.github.com>
2024-10-30 08:11:50 +0000

    string funtions

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,5 +1,21 @@
-const name = "Omkar"
+const name = "Omkar-Nath"
 const namel = "Nath"
 
 console.log(`Hello my first name is ${name} and my last name is ${namel}.`);
-//start ftom 12:27
+
+const gameName = new String ('Omkar-Nath')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toLocaleUpperCase());
+
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('m'));
+
+const newString = gameName.substring(0,4);
+// console.log(newString);
+
+const anothorString = gameName.slice(-8, 4)
+console.log(anothorString);
