133447fdb57a425bb19a3f9ee11f36194b4c833e
Omkar Nath <71923429+nathomkar799@users.noreply.github.com>
2025-01-07 13:26:26 +0000

    scopes and hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,7 +9,7 @@ if (true){
 }
 
 
-console.log(a); //a is not defined
+// console.log(a); //a is not defined
 // console.log(b); //b is not defined
 // console.log(c);//30
 
@@ -19,3 +19,44 @@ console.log(a); //a is not defined
 //the global scope of browser inspect is different and the code environment global scope is different. 
 
 
+function one (){
+    const username = "Omkar"
+
+    function two (){
+        const website = "YouTube"
+        console.log(username);
+    }
+    // console.log(website);//It gives error because website is in two so now its not defined
+
+    two()//gives username
+    
+}
+
+// one()
+
+if (true) {
+    const username = "Omkar"
+
+    if (username === "Omkar") {
+        const website = "YouTube"
+        // console.log(username + website);
+    }
+    // console.log(website);   
+}
+
+// console.log(username);
+
+
+//++++++++++++++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++
+
+addone(5)//this will not give error
+function addone (num) {
+    return num + 1
+}
+
+
+addTwo(5)//this will give error because Cannot access 'addTwo' before initialization as this function is created as a holding in a variable also known as hoisting
+
+const addTwo = function(num){
+    return num + 2
+}
