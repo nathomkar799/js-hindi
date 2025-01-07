f3f07edf1b5158dc8a6abe288a28439f04ee7ed6
Omkar Nath <71923429+nathomkar799@users.noreply.github.com>
2025-01-07 10:12:55 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,21 @@
+// var c = 300
+
+let a = 300
+
+if (true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+
+console.log(a); //a is not defined
+// console.log(b); //b is not defined
+// console.log(c);//30
+
+//never use var beacause once it is used can be changed in the block or in global.
+
+
+//the global scope of browser inspect is different and the code environment global scope is different. 
+
+
